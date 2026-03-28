2b972c4f821986c18cee4ff5ed929bffc27d9cd3
Rahul Manna <r4236499@gmail.com>
2026-03-29 00:38:47 +0530

    Add break and continue statements and refactors

--- All basics.cpp
+++ All basics.cpp
@@ -10,10 +10,11 @@ int main() {
 	
 	std::cout<<"i am a man \n";   // use when (  using namespace std ) not mentioned 
     cout <<"i am learning cpp\n";  //printing 
-    
+    printf("hello! i am rahul");   // use case of cstudio
+	
     int rollnumber= 23;  
-    cout<<"my roll no is : " <<rollnumber << endl;        //endl is for instant data flash , slower then \n
-
+    cout<<"\nmy roll no is : " <<rollnumber << endl;        //endl is for instant data flash , slower then \n
+    printf("\nmy roll no is %d",rollnumber);     //usecase of cstudio 
     
 //    string name = "Rahul";
 //     int roll = 23;
@@ -64,20 +65,27 @@ int main() {
 
 
     // 4. If-Else
+	int num2;
     cout << "\nIf-Else:\n";
-    if (num % 2 == 0) {
+	cout<<"enter the number to cheak even or odd: ";
+	cin>> num2;
+    if (num2 % 2 == 0) {
         cout << "Even Number\n";
     } else {
         cout << "Odd Number\n";
     }
 
-// Ternary Operator            //condition ? value_if_true : value_if_false;
-cout << (age >= 18 ? " You are Adult" : " You are Minor");     
+	// Ternary Operator            //condition ? value_if_true : value_if_false;
+	cout << (age >= 18 ? " You are Adult" : " You are Minor");     
+	int temp;
+	cout << "\nenter the temperature today: ";
+	cin >> temp;
+	cout << (temp<= 10 ? "verycold" : temp <=20 ? "normal" : temp<=30 ? "worm" : "very worm");    //best application of ternary operator
 
     // 6. Switch Case
     cout << "\nSwitch Case:\n";
     int choice;
-    cout << "Enter 1(Add), 2(Sub): ";
+    cout << "1. Add\n2. Sub\nenter your choice: ";
     cin >> choice;
 
     switch(choice) {
@@ -93,29 +101,39 @@ cout << (age >= 18 ? " You are Adult" : " You are Minor");
 
 
     // 7. For Loop
-    cout << "\nFor Loop:\n";
-    for (int i = 1; i <= 5; i++) {
+    cout << "\nFor Loop:\n";   
+    for (int i = 1; i <= 5; i++) {      //for(; ;) infinite loop  break is the exit condition
         cout << i << " ";
     }
 
 
     //  8. While Loop
-    cout << "\n\nWhile Loop:\n";
+    cout << "\n\nWhile Loop:\n";    //cheak condition then do 
     int i = 1;
-    while (i <= 5) {
+    while (i <= 5) {                  // while(1) or while(true) for infinite loop   break is exit condition
         cout << i << " ";
         i++; 
     }
 
 
     //  9. Do-While Loop
-    cout << "\n\nDo-While Loop:\n";
+    cout << "\n\nDo-While Loop:\n";      // first do , then cheak condition
     int j = 1;
     do {
         cout << j << " ";
         j++;
     } while (j <= 5);
 
-
+	//break and continue 
+	i=0;
+	while (i < 10) {
+        i++; // Increment i before the condition to avoid an infinite loop
+        if (i == 3) {  // Skip the current iteration when i is 3
+            continue;  //  ans : 0 1 2 4 5 6 7 8 9
+			//break;     // ans: 0 1 2 
+        }
+		cout << i << " ";   //printing 
+    }
+		
     return 0;
 }
