195d2c3ba63fa4c8c1fa333e78d73a389d6b53fb
Rahul Manna <r4236499@gmail.com>
2026-04-08 08:46:06 +0530

    Add priority queue demonstration in queue_Pqueue_dequeue.cpp

--- STL_FILES/queue_Pqueue_dequeue.cpp
+++ STL_FILES/queue_Pqueue_dequeue.cpp
@@ -20,5 +20,21 @@ while(!q.empty()){
     q.pop();
 }
 cout<<endl <<q.size() <<" " <<temp.size() << " " <<temp.front() <<" "<< temp.back();
+    
+
+
+priority_queue<int> pq;
+
+pq.push(23);
+pq.push(67);
+pq.push(3);
+
+priority_queue<int> temp2 = pq;
+while(!pq.empty()){
+    cout<<pq.top() <<" ";
+    pq.pop();
+}
+
+cout<<endl <<pq.size() <<" " <<temp2.size() << " " <<temp2.top();
     return 0;
 }
