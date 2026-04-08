21718d66c69370d363c49d652bfbfc630524432f
Rahul Manna <r4236499@gmail.com>
2026-04-08 08:55:39 +0530

    Add deque functionality to queue_Pqueue_dequeue.cpp

--- STL_FILES/queue_Pqueue_dequeue.cpp
+++ STL_FILES/queue_Pqueue_dequeue.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include<queue>
+#include<dequeue>
 using namespace std;
 int main() {
 queue<int> q;
@@ -36,5 +37,33 @@ while(!pq.empty()){
 }
 
 cout<<endl <<pq.size() <<" " <<temp2.size() << " " <<temp2.top();
+
+
+deque<int> dq;
+deque<int> dq2;
+
+dq.push_front(12);  //12
+dq.push_front(23);  //23 12
+dq.push_front(21);  // 21 23 12
+dq.push_front(3);   // 3 21 23 12
+
+
+dq2.push_back(12);  //12
+dq2.push_back(23);  //12 23 
+dq2.push_back(21);  // 12 23 21
+dq2.push_back(3);   // 12 23 21 3
+
+
+while(!dq.empty()){
+    cout<<dq.front()<<" ";  // 3 21 23 12
+    dq.pop_front();
+}
+cout <<endl;
+
+while(!dq2.empty()){
+    cout<<dq2.back()<<" ";  // 3 21 23 12
+    dq2.pop_back();
+}
+    
     return 0;
 }
