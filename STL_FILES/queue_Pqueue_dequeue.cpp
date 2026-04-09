10a3744108676edeed5b26af699525c4c44ec760
Rahul Manna <r4236499@gmail.com>
2026-04-09 07:58:19 +0530

    Refactor queue and priority queue operations
    Refactored queue and priority queue operations for clarity. Improved comments and organized output formatting.

--- STL_FILES/queue_Pqueue_dequeue.cpp
+++ STL_FILES/queue_Pqueue_dequeue.cpp
@@ -1,68 +1,67 @@
 
 #include <iostream>
-#include<queue>
-#include<dequeue>
+#include<queue>        //for queue and priority queue operations
+#include<dequeue>     //double side queue operations 
 using namespace std;
 int main() {
-queue<int> q;
 
-q.push(10);
+cout <<"===========================================  QUEUE OPERATIONS =============================================================="
+queue<int> q;       //  define a int type queue name q;
+q.push(10);   //pushing in queue
 q.push(20);
 q.push(4);
 q.push(205);
 q.push(23);
-q.push(57);
-q.push(47);
-q.push(18);
 
-queue<int> temp = q;
-while(!q.empty()){
-    cout<<q.front() <<" ";
-    q.pop();
+queue<int> temp = q;  //create a another queue whwre all eliments of q is saved
+while(!q.empty()){   // while not empty print 
+    cout<<q.front() <<" ";   // return queue front 
+    q.pop();     // remove the front 
 }
-cout<<endl <<q.size() <<" " <<temp.size() << " " <<temp.front() <<" "<< temp.back();
+cout<<endl <<q.size() <<" " <<temp.size() << " " <<temp.front() <<" "<< temp.back();    //return size  return front and back 
     
 
-
-priority_queue<int> pq;
+cout <<"================================== PRIORITY QUEUE OPERATIONS ======================================================"
+priority_queue<int> pq;    // priority queue define
 
 pq.push(23);
-pq.push(67);
+pq.push(67);   //push is same like normal queue 
 pq.push(3);
 
-priority_queue<int> temp2 = pq;
+priority_queue<int> temp2 = pq;    // copying in temp2 priority queue 
 while(!pq.empty()){
-    cout<<pq.top() <<" ";
-    pq.pop();
+    cout<<pq.top() <<" ";    //return front eliment 
+    pq.pop();       // remove the front
 }
 
-cout<<endl <<pq.size() <<" " <<temp2.size() << " " <<temp2.top();
+cout<<endl <<pq.size() <<" " <<temp2.size() << " " <<temp2.top();   // return size    and top => returns the front
 
+cout <<"================================== DOUBLE SIDE  QUEUE OPERATIONS ======================================================"
 
-deque<int> dq;
+deque<int> dq;     //double ended queue
 deque<int> dq2;
 
-dq.push_front(12);  //12
+dq.push_front(12);  //12       pushing operations from front
 dq.push_front(23);  //23 12
 dq.push_front(21);  // 21 23 12
 dq.push_front(3);   // 3 21 23 12
 
 
-dq2.push_back(12);  //12
+dq2.push_back(12);  //12   //pushing from back 
 dq2.push_back(23);  //12 23 
 dq2.push_back(21);  // 12 23 21
 dq2.push_back(3);   // 12 23 21 3
 
 
 while(!dq.empty()){
-    cout<<dq.front()<<" ";  // 3 21 23 12
-    dq.pop_front();
+    cout<<dq.front()<<" ";  // 3 21 23 12    //print fromo front side 
+    dq.pop_front();    // pop from front 
 }
 cout <<endl;
 
 while(!dq2.empty()){
-    cout<<dq2.back()<<" ";  // 3 21 23 12
-    dq2.pop_back();
+    cout<<dq2.back()<<" ";  // 3 21 23 12    print from backside 
+    dq2.pop_back();     //  pop from back 
 }
     
     return 0;
