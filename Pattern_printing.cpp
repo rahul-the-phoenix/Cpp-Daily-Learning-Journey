08bf297be63306ab17a57ee0535b43597b0c60dc
Rahul Manna <r4236499@gmail.com>
2026-05-06 00:00:20 +0530

    Add pattern printing for medium difficulty level

--- Pattern_printing.cpp
+++ Pattern_printing.cpp
@@ -102,6 +102,22 @@ for(int i=0;i<n;i++){
      cout << "\n";
  }
 
+// medium 2:
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+ for(int i=0;i<n;i++){
+     for(int j=0;j<i;j++){
+         cout << " ";
+     }
+     for( int j = 0;j<2*(n-i)-1;j++){
+         cout <<"*";
+     }
+     cout << "\n";
+ }
+
 
 
 
