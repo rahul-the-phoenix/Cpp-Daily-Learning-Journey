eb9d890f2360c11b353f07586bbdbdb29a1c840d
Rahul Manna <r4236499@gmail.com>
2026-05-05 18:05:21 +0530

    Add pattern printing for right angle triangle
    Implement pattern printing for right angle triangle.

---
+++ Pattern_printing.cpp
@@ -0,0 +1,13 @@
+//  this is for the right angle triangle 
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * * 
+
+for( int i=1;i<=n;i++){                 //    for print endl
+    for(int j=1;j<=i;j++){                        //  for print *
+        cout <<"*"<<" ";
+    }
+    cout <<endl;
+}
