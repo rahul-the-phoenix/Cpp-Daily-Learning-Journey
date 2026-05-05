618990a4172fe55e06b698cf62218fade091d7cf
Rahul Manna <r4236499@gmail.com>
2026-05-05 22:57:17 +0530

    Change pattern from asterisks to numbers
    Updated pattern printing logic to print numbers instead of asterisks.

--- Pattern_printing.cpp
+++ Pattern_printing.cpp
@@ -1,13 +1,12 @@
-//  this is for the right angle triangle 
-// * 
-// * * 
-// * * * 
-// * * * * 
-// * * * * * 
-
-for( int i=1;i<=n;i++){                 //    for print endl
-    for(int j=1;j<=i;j++){                        //  for print *
-        cout <<"*"<<" ";
+// easy 1 : 
+// 1 
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+// 1 2 3 4 5 
+for(i=1;i<=n;i++){ 
+    for(j=1;j<=i;j++){
+        cout <<i <<" ";
     }
-    cout <<endl;
+    cout <<"\n";
 }
