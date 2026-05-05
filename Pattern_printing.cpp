57215cbff2740d68d1e5dafcce8b561dca1cb4f3
Rahul Manna <r4236499@gmail.com>
2026-05-06 00:36:38 +0530

    Add pattern printing for stars and numbers

--- Pattern_printing.cpp
+++ Pattern_printing.cpp
@@ -133,3 +133,41 @@ for(int i=0;i<n;i++){
 
 
 
+// medium 3
+// *
+// **
+// ***
+// ****
+// *****
+// ****
+// ***
+// **
+// *
+for(int i=1;i<=2*n-1;i++){
+     int star=i;
+    if(star>n) star=2*n-i;   // using if else where needed 
+    
+    for(int j= 1;j<=star;j++){
+        cout <<"*";
+    } 
+    cout <<"\n";
+}
+
+// medium 4 
+// 1      1
+// 12    21
+// 123  321
+// 12344321
+for(int i=1;i<=n;i++){
+    for(int j= 1;j<=i;j++){
+        cout <<j;
+    }
+    for(int j= 1;j<=2*(n-i);j++){
+        cout <<" ";
+    }
+    for(int j= i;j>=1;j--){
+        cout <<j;
+    }
+ 
+    cout <<"\n";
+}
