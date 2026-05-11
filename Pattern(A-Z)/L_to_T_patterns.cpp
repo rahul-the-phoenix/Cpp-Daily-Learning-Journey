3c09fa4cbbf89f878f34adc98eee5b98e8db1fd8
Rahul Manna <r4236499@gmail.com>
2026-05-11 16:56:28 +0530

    Improve pattern printing in L_to_T_patterns.cpp
    Refactor pattern printing logic for clarity and structure.

--- Pattern(A-Z)/L_to_T_patterns.cpp
+++ Pattern(A-Z)/L_to_T_patterns.cpp
@@ -7,6 +7,23 @@ for(int i=1;i<=n;i++){
     cout<<"* ";
 }
 
+//===============================MMMMMMMMMMMMMMMMMMMMMMMM=========================
+
+for(int i=1;i<=n/2+1;i++){
+    for(int j=1;j<=n;j++){
+        if(j==1 || j==i || j==(n-i+1) || j==n) cout<<"*";
+        else cout <<" ";
+    }
+    cout << endl;
+}
+for(int i= n/2+2;i<=n;i++){
+    for(int j=1;j<=n;j++){
+        if(j==1 || j==n) cout <<"*";
+        else cout <<" ";
+    }
+    cout << endl;
+}
+
 
 //==============================NNNNNNNNNNNNNNNNNNNNNN===========================
 for(int i=1;i<=n;i++){
