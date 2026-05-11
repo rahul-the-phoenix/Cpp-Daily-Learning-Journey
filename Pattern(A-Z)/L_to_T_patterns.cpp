9ebf7d27c2aed3e0c18f7dba99a4b5245731754c
Rahul Manna <r4236499@gmail.com>
2026-05-11 16:22:33 +0530

    Implement pattern printing with nested loops
    Add nested loop to print a pattern with asterisks.

--- Pattern(A-Z)/L_to_T_patterns.cpp
+++ Pattern(A-Z)/L_to_T_patterns.cpp
@@ -6,3 +6,13 @@ for( int i=1;i<n ;i++){
 for(int i=1;i<=n;i++){
     cout<<"* ";
 }
+
+
+//==============================NNNNNNNNNNNNNNNNNNNNNN===========================
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=n;j++){
+        if(j==1 || j==n || j==i) cout<<"*";
+        else cout <<" ";
+    }
+    cout << endl;
+}
