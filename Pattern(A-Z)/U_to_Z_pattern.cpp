06a9c3a9af9a6b0ed4574201248669dd9abbbc08
Rahul Manna <r4236499@gmail.com>
2026-05-11 18:09:36 +0530

    Fix pattern generation loop in U_to_Z_pattern.cpp

--- Pattern(A-Z)/U_to_Z_pattern.cpp
+++ Pattern(A-Z)/U_to_Z_pattern.cpp
@@ -1,1 +1,10 @@
 
+
+//==========================VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV===============================
+for(int i=1;i<=n/2+1;i++){
+    for( int j=1;j<=n;j++){
+        if(j==i || j== n-i+1) cout <<"*"; else cout << " ";
+    }
+    cout << endl;
+}
+
