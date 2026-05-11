ce02ab7b7890a39e84f5fcab5ee88df8b854c96b
Rahul Manna <r4236499@gmail.com>
2026-05-11 18:00:13 +0530

    Add T pattern printing functionality

--- Pattern(A-Z)/L_to_T_patterns.cpp
+++ Pattern(A-Z)/L_to_T_patterns.cpp
@@ -104,3 +104,13 @@ for(int i=1;i<=n;i++){
     }
     cout << endl;
 }
+
+//====================TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT===============================
+for( int i=1;i<=n;i++){
+cout <<"*";
+}
+cout << endl;
+for( int i=2;i<=n;i++){
+    for( int j=1;j<=n;j++) if(j==n/2+1) cout <<"*"; else cout <<" ";
+    cout << endl;
+}
