0f34a484e1375cfeadc509ed3220d6c01ac517ec
Rahul Manna <r4236499@gmail.com>
2026-05-11 18:10:56 +0530

    Add pattern printing for U to Z shape

--- Pattern(A-Z)/U_to_Z_pattern.cpp
+++ Pattern(A-Z)/U_to_Z_pattern.cpp
@@ -8,3 +8,10 @@ for(int i=1;i<=n/2+1;i++){
     cout << endl;
 }
 
+//================================XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX============================
+for(int i=1;i<=n;i++){
+    for( int j=1;j<=n;j++){
+        if(j==i || j== n-i+1) cout <<"*"; else cout << " ";
+    }
+    cout << endl;
+}
