5f163f192c3a25b4b516357e12de27c38aa8f8be
Rahul Manna <r4236499@gmail.com>
2026-05-12 17:16:37 +0530

    Add pattern generation for U to Z shapes

--- Pattern(A-Z)/U_to_Z_pattern.cpp
+++ Pattern(A-Z)/U_to_Z_pattern.cpp
@@ -15,3 +15,13 @@ for(int i=1;i<=n;i++){
     }
     cout << endl;
 }
+
+//========================ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ=========================
+for(int i=1;i<=n;i++){
+     for( int j=1;j<=n;j++){
+         
+         if(i==n || i==1 || j==n-i+1) cout<<"*";
+         else cout <<" ";
+     }
+     cout << endl;
+ }
